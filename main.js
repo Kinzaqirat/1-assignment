36aab2bae1d1a6f5518ec95a3678f720c122ca43
[Qiratsumra] <[qiratsaeed5@gmail.com]>
2024-03-05 00:18:50 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var personName = "kinza";
+var message = "would you like to learn some Python today?”";
+console.log(personName, message);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let personName : string = "kinza";
+let message : string = "would you like to learn some Python today?”";
+console.log(personName,message)
